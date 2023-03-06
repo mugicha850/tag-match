182a1eefc2ccddfab0ac75320a91f5180e67d560
mugicha850 <tomo.fh.123@gmail.com>
2023-03-06 12:54:32 +0900

    ユーザー新規登録ページの作成 homeコントローラーの削除、userコントローラの作成

---
+++ app/controllers/external_coach/users_controller.rb
@@ -0,0 +1,68 @@
+class ExternalCoach::UsersController < ExternalCoach::Base
+  def index
+    @external_coach = ExternalCoach.order(:family_name_kana, :given_name_kana)
+  end
+
+  def show
+    @external_coach = ExternalCoach.find(params[:id])
+    redirect_to [ :edit, :admin, external_coach ]
+  end
+
+  def new
+    if current_external_coach
+      redirect_to :external_coach_root
+    else
+      @external_coach = ExternalCoach.new
+      render action: "new"
+    end
+  end
+
+  def edit
+    @external_coach = ExternalCoach.find(params[:id])
+  end
+
+  def update
+    @external_coach = ExternalCoach.find(params[:id])
+    @external_coach.assign_attributes(params[:external_coach])
+    if @external_coach.save
+      flash.notice = "職員アカウントを更新しました。"
+      redirect_to :external_coach_users
+    else
+      render action: "edit"
+    end
+  end
+ 
+  def create
+    @external_coach = ExternalCoach.new(params[:external_coach])
+    if @external_coach.save
+      flash.notice = "職員アカウントを新規登録しました。"
+      #redirect_to :external_coach_root
+
+      @form = ExternalCoach::LoginForm.new(new_form_params)
+      if @form.email.present?
+        external_coach =
+          ExternalCoach.find_by("LOWER(email) = ?", @form.email.downcase)
+      end
+        if ExternalCoach::Authenticator.new(external_coach).authenticate(@form.password)
+          session[:external_coach_id] = external_coach.id
+          flash.notice = "ログインしました。"
+          redirect_to :external_coach_root
+        end
+
+    else
+      render action: "new"
+    end
+  end
+
+  def destroy
+    external_coach = ExternalCoach.find(params[:id])
+    external_coach.destroy!
+    flash.notice = "職員アカウントを削除しました。"
+    redirect_to :external_coach_root
+  end
+
+  private def new_form_params
+    params.require(:external_coach).permit(:email, :password)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,16 @@
 Rails.application.routes.draw do
   namespace :external_coach do
-    root "home#index"
+    root "users#index"
     get "login" => "sessions#new", as: :login
     post "session" => "sessions#create", as: :session
     delete "session" => "sessions#destroy"
 
-    get "test" => "home#test"
 
     # profileルーティング
     get "profile" => "profiles#show"
+
+    # userルーティング
+    resources :users
 
     # accountルーティング
     resource :account, only: [:show, :edit, :update]
