7652bd15c2f15a1366a90e306d1c5a788aa7828b
mugicha850 <tomo.fh.123@gmail.com>
2023-05-18 21:46:12 +0900

    エラーページ作成 開発環境と本番環境の使い分け

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,16 @@
 class ApplicationController < ActionController::Base
   layout :set_layout
 
+  # アクセス権限がない時などのエラー処理クラスの定義
   class Forbidden < ActionController::ActionControllerError; end
+
+  #特定のIPアドレスからのリクエストをアプリケーションで拒否する必要がある場合などのエラー処理クラスの定義
   class IpAddressRejected < ActionController::ActionControllerError; end
 
-  # 例外エラー処理クラスの定義
+  # アクセスが制限されたリソースにアクセスしようとした場合や、ログインしていないユーザーが特定の機能を利用しようとした場合などのエラー処理クラスの定義
   class LoginRequired < StandardError; end
 
-#  include ErrorHandlers if Rails.env.production?
+  # include ErrorHandlers if Rails.env.production?
 
   private def set_layout
     if params[:controller].match(%r{\A(external_coach|club_advisor)/})
@@ -16,4 +19,27 @@ class ApplicationController < ActionController::Base
       "club_advisor"
     end
   end
+
+  # エラー(ステータスコード500)の発生時に特定の処理を行う
+  rescue_from StandardError, with: :rescue500
+
+  # 閲覧する権限がないなどのエラー(ステータスコード403)の発生時に特定の処理を行う
+  rescue_from Forbidden, with: :rescue403
+
+  # IPアドレス制限によるアクセス拒否などのエラー(ステータスコード403)の発生時に特定の処理を行う
+  rescue_from IpAddressRejected, with: :rescue403
+  
+  # ステータスコード500のエラーページを表示するための処理を行う
+  private def rescue500(e)
+    render "errors/internal_server_error", status: 500
+  end
+
+  # ステータスコード403のエラーページを表示するための処理を行う
+  private def rescue403(e)
+    # rescue403メソッドで受け取ったエラーオブジェクト(e)をインスタンス変数@exceptionに代入しています。
+    @exception = e
+    render "errors/forbidden", status: 403
+  end
+
+
 end
--- app/controllers/club_advisor/articles_controller.rb
+++ app/controllers/club_advisor/articles_controller.rb
@@ -2,9 +2,9 @@ class ClubAdvisor::ArticlesController < ClubAdvisor::Base
   
   # 外部コーチ:記事一覧
   def index
-      @articles = EcArticle.common
-      .visible.order(released_at: :desc)
-      .page(params[:page]).per(5)
+    @articles = EcArticle.common
+    .visible.order(released_at: :desc)
+    .page(params[:page]).per(5)
   end
 
   # 外部コーチ:記事詳細
